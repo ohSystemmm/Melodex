60544d37f76126ea8f154350a2f34c2a0a96dfd5
ohSystemmm <leonhardfresacher@gmail.com>
2024-08-31 12:43:35 +0200

    Removing unnecessary shit

--- Melodex-UserSphere/src/Validator.java
+++ Melodex-UserSphere/src/Validator.java
@@ -3,11 +3,9 @@ import java.util.regex.Matcher;
 
 public class Validator {
 
-    private final String regex;
     private final Pattern pattern;
 
     public Validator(String regex) {
-        this.regex = regex;
         this.pattern = Pattern.compile(regex);
     }
 
