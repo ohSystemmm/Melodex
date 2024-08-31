918c45ac6facd6ca087a536ba20e4614e0230d3a
ohSystemmm <leonhardfresacher@gmail.com>
2024-08-31 12:40:12 +0200

    Regex added

--- Melodex-UserSphere/src/Main.java
+++ Melodex-UserSphere/src/Main.java
@@ -1,13 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        User Y1kun = new User();
-        User Y2kun = new User();
-        User Y3kun = new User();
+        System.out.println("Melodex");
 
-        Y3kun.addUser("Y2kun", "1", "IamGay", "https://gaysex.com");
+        // Debug values for now
+        String username = "Test1234";
+        checkInput("^[A-Za-z][A-Za-z0-9]*$", username);
 
+        String clientID = "7e4a9c13b77d46b9a77c2e5f9d6a1a8b";
+        checkInput("^[a-fA-F0-9]{32}$", clientID);
+
+        String clientSecret = "3f2b7d9c8a9e4b1c9f9d6e2a8c4d7e5a";
+        checkInput("^[a-fA-F0-9]{32}$", clientSecret);
+
+        String redirectURI = "http://localhost:1234/callback";
+        checkInput("^http://localhost:\\d+/callback$", redirectURI);
+
+        // Debug output
+        System.out.print("\n");
+        System.out.println("Username: " + username);
+        System.out.println("Client ID: " + clientID);
+        System.out.println("Client Secret: " + clientSecret);
+        System.out.println("Redirect URI: " + redirectURI);
+
+        Users users = new Users();
+        users.addUser(username, clientID, clientSecret, redirectURI);
+    }
+
+    static String input(String message) {
+        Scanner input = new Scanner(System.in);
+        System.out.print(message);
+        return input.nextLine();
+    }
+
+    static void checkInput(String regex, String content) {
+        Validator validator = new Validator(regex);
+        if (!validator.checkRegex(content)) {
+            throw new CustomException("Invalid Input: " + content);
+        }
     }
 }
---
+++ Melodex-UserSphere/src/Validator.java
@@ -0,0 +1,23 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Validator {
+
+    private final String regex;
+    private final Pattern pattern;
+
+    public Validator(String regex) {
+        this.regex = regex;
+        this.pattern = Pattern.compile(regex);
+    }
+
+    public boolean checkRegex(String message) {
+        if (message == null) {
+            throw new NullPointerException("Message cannot be null");
+        }
+        Matcher matcher = pattern.matcher(message);
+        return matcher.matches();
+    }
+}
+
+
